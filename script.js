f94f05a05872b49d0577eb9d099f34ce95acaf8f
monskvll <devmonskvll@gmail.com>
2020-12-17 18:11:44 +0000

    Grid creation + painting functions and clear button addade.

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-
 function createGridSquare() {
-    for(i = 0; i < 256; i++) {
-        let gridSquare = document.createElement('div');
+    let gridSize = 16*16;
+
+    for(i = 0; i < gridSize; i++) {        
+        let gridSquare = document.createElement('div');        
         gridSquare.setAttribute('class', 'square');
-        container.appendChild(gridSquare);        
-    }  
+        container.appendChild(gridSquare);
+
+        gridSquare.addEventListener("mouseenter", function (event) {
+            gridSquare.classList.add('square-test');    
+        });
+
+        document.getElementById("clearButton").addEventListener("click", function (event) {
+            gridSquare.classList.remove('square-test')
+        })
+    }
 }
 
-gridSquare.addEventListener("mouseenter", function (event) {
-    event.target.style.backgroundColor = 'black';
-
-    setTimeout(function() {
-        event.target.style.backgroundColor = '';
-    }, 500);
-}, false);
+function clearButton() {
+    let gridInput = prompt("Choose a grid size (max. 100x100).", "0");
+    let  gridSize = parseInt(gridInput + gridInput);
+    return gridSize;
+}
 
 createGridSquare();
-
