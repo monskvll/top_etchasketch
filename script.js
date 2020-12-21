cd898a1ff7fb60d52bdd7f909d79d2121f802931
monskvll <devmonskvll@gmail.com>
2020-12-21 14:35:56 +0000

    Refactoring functions and CSS.

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-function createGridSquare() {
-    let gridSize = 16*16;
+const container = document.querySelector('#container');
+const reset = document.querySelector('#reset-button');
 
-    for(i = 0; i < gridSize; i++) {        
+window.addEventListener('load', setDefaultGrid);
+reset.addEventListener('click', changeSize)
+
+
+function setDefaultGrid() {
+    setGridSize(16);
+    fillGrid(16);
+}
+
+function setGridSize(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;        
+}
+
+function fillGrid(size) {
+
+    for(i = 0; i < size*size; i++) {        
         let gridSquare = document.createElement('div');        
         gridSquare.setAttribute('class', 'square');
         container.appendChild(gridSquare);
-
+        
         gridSquare.addEventListener("mouseenter", function (event) {
-            gridSquare.classList.add('square-test');    
+            gridSquare.classList.add('square-active');    
         });
 
-        document.getElementById("clearButton").addEventListener("click", function (event) {
-            gridSquare.classList.remove('square-test')
-        })
     }
 }
 
-function clearButton() {
-    let gridInput = prompt("Choose a grid size (max. 100x100).", "0");
-    let  gridSize = parseInt(gridInput + gridInput);
-    return gridSize;
+function changeSize() {
+    let newSize = prompt("Choose a new grid size (1-100).");
+
+    if (newSize != null) {
+        newSize = parseInt(newSize);
+
+        if (newSize < 1 || newSize > 100 ||  Number.isNaN(newSize)) {
+            alert("Enter a number between 1 and 100.")
+            changeSize();
+        } else {
+            clearGrid();
+            setGridSize(newSize);
+            fillGrid(newSize);
+        }
+    }
 }
 
-createGridSquare();
+function clearGrid() {    
+    const gridSquaresArray = Array.from(container.childNodes);
+    gridSquaresArray.forEach((container) => {
+        container.classList.remove('square-active');
+    });    
+}
